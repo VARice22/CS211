855d4b31acb08b6d060a63a36b4f35b74cf6d646
Aaron V <38258274+VARice22@users.noreply.github.com>
2018-11-27 17:07:50 -0500

    Add files via upload

---
+++ RAM.java
@@ -0,0 +1,27 @@
+package hw2;
+
+public class RAM {
+    private int[] rs;
+    
+    public RAM(int size, int bytes){
+        rs = new int[size*8*bytes];
+    }
+    
+    public Word get(int bytes, int a){
+        int bits = 8*bytes;
+        int[] r = new int[bits];
+        for(int i = 0; i < bits; i++){
+            r[i] = rs[a+i];
+        }
+        Word w = new Word(bits);
+        w.set(r);
+        return w;       
+    }
+    
+    public void set(Word w, int a){
+        int[] r = w.get();
+        for(int i = 0; i < r.length; i++){
+            rs[a+i] = r[i];
+        }
+    }
+}
