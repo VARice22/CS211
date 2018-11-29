7379b2f1ea237172f206d8d73b0965eecfddadfd
Aaron V <38258274+VARice22@users.noreply.github.com>
2018-11-29 15:37:39 -0500

    Update convert.java

--- ARM_EMU/src/arm_emu/convert.java
+++ ARM_EMU/src/arm_emu/convert.java
@@ -10,51 +10,59 @@ package arm_emu;
  * @author Sharo
  */
 public class convert {
-    int[] dtc(int value,int bits)
+        boolean[] dtc(int value,int bits)//decimal to two complement
     {
-        int[] t=new int[bits];
+        boolean[] t=new boolean[bits];
         if (value>=0)
         {
-            t[0]=0;
+            t[0]=false;
             for(int i=1; i<bits; i++)
             {
                 if (value>=Math.pow(2,((bits-1)-i)))
                 {
-                    t[i]=1;
+                    t[i]=true;
                     value-=Math.pow(2,((bits-1)-i));
                 }
-                else t[i]=0;
+                else t[i]=false;
             }
             return t;
         }
         else {
-            t[0]=1;
+            t[0]=false;
             value=(value*-1)-1;
             
             for(int i=1; i<bits; i++)
             {
                 if (value>=Math.pow(2,((bits-1)-i)))
                 {
-                    t[i]=0;
+                    t[i]=false;
                     value-=Math.pow(2,((bits-1)-i));
                 }
-                else t[i]=1;
+                else t[i]=true;
             }
             return t;
         }
     }
-    int[] atc(int[] t, int[] y)
+    boolean[] atc(boolean[] t, boolean[] y)// add twos complement
     {
-        int carry=0;
-        int[] k=new int[t.length];
+        boolean carry=false;
+        boolean[] k=new boolean[t.length];
         
         for(int i=k.length-1; i>=0;i--)
         {
+            /*
             if(t[i]+y[i]+carry==0){k[i]=0; carry=0;}
             else if(t[i]+y[i]+carry==1){k[i]=1; carry=0;}
             else if(t[i]+y[i]+carry==2){k[i]=0; carry=1;}
             else if(t[i]+y[i]+carry==3){k[i]=1; carry=1;}
+            */
+            if(t[i]==false && y[i]==false && carry==false){k[i]=false; carry=false;}//0
+            else if(t[i] && y[i] && carry){k[i]=true; carry=true;}//3
+            else{
+                if(t[i]&&y[i] || carry&&y[i] || carry&&t[i]){k[i]=false; carry=true;}//2
+                else /*if(!t[i]&&!y[i] || !carry&&!y[i] || !carry&&!t[i])*/{k[i]=true; carry=false;}//1
+            }
         }
-        return k;   
+        return k;
     }
 }
